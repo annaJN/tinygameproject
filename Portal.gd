fc4d27dc66357720b91623d9b023a630e3f9fcf3
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-03-23 18:04:50 +0100

    Level 1
    Added a portal going back and forth between "home" and level 1

---
+++ Portal.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _on_body_entered(body):	
+	if body.name == "Player":
+		if str(get_tree().current_scene.name) == "Game":
+			get_tree().change_scene_to_file("res://LevelOne.tscn")
+			
+		elif str(get_tree().current_scene.name) == "LevelOne":
+			get_tree().change_scene_to_file("res://Game.tscn")
+
