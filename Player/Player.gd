29ea3703a3b50f2f237d6f002d3ab4b0b600c154
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-03-26 11:47:11 +0100

    Walls and restructure

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var ground
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
