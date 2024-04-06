7c28220723febfd9ef646b00b0685736b8bb8159
stihan01 <stihan01@gmail.com>
2024-04-06 15:54:17 +0200

    Added double jump

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,16 +7,20 @@ const JUMP_VELOCITY = -200.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var ground
+var jump_count = 0
+var max_jumps = 2
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	
+	if is_on_floor():
+		jump_count = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
