0b6587f6e8f5ecf06a804bceda7c763bf179ffa1
stihan01 <stihan01@gmail.com>
2024-04-06 16:46:33 +0200

    added wall jump+sliding

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,7 +9,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var ground
 var jump_count = 0
 # could be increased if we want a power up or for accessibly purposes
+#if we don't want to have double jump, change it to 1
 var max_jumps = 2
+
+const wall_jump_push = 100
+const wall_slide_gravity = 50
+var is_wall_sliding = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,9 +25,15 @@ func _physics_process(delta):
 	if is_on_floor():
 		jump_count = 0
 	# Handle jump (including double jump)
-	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jumps:
-		velocity.y = JUMP_VELOCITY
-		jump_count += 1
+	if Input.is_action_just_pressed("ui_accept"):
+		if jump_count < max_jumps:
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
+		wall_jump()
+		
+	wall_sliding(delta)	
+		
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -40,3 +51,24 @@ func _input(event):
 				# self.position.x += 50
 				var tween = get_tree().create_tween()
 				tween.tween_property(self, "position", position + Vector2(50,0), 0.1)
+
+#handles wall sliding, makes it so gravity is slower when you are climbing a wall
+func wall_sliding(delta):
+	if is_on_wall() and !is_on_floor():
+		if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+			is_wall_sliding=true
+		else:
+			is_wall_sliding = false
+		
+		if is_wall_sliding:
+			velocity.y += (wall_slide_gravity*delta)
+			velocity.y = min(velocity.y, wall_slide_gravity)
+	
+#handles wall jumping, makes it so you can jump (for now infinitely) on a wall
+func wall_jump():
+	if Input.is_action_pressed("ui_right") and is_on_wall():
+		velocity.y = JUMP_VELOCITY
+		velocity.x = -wall_jump_push
+	if Input.is_action_pressed("ui_left") and is_on_wall():
+		velocity.y = JUMP_VELOCITY
+		velocity.x = wall_jump_push
