a3cf083f028e4ddef815810aee29cd3bd17019c0
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-04-12 18:02:23 +0200

    broke the portal, which is now fixed. checked to the save functionality works as it should

--- Objects/Portal.gd
+++ Objects/Portal.gd
@@ -2,9 +2,9 @@ extends Area2D
 
 func _on_body_entered(body):	
 	if body.name == "Player":
-		if str(get_tree().current_scene.name) == "Game":
+		if str(get_tree().current_scene.name) == "Home":
 			get_tree().change_scene_to_file("res://Views/Levels/LevelOne.tscn")
 			
 		elif str(get_tree().current_scene.name) == "LevelOne":
-			get_tree().change_scene_to_file("res://Views/Levels/Game.tscn")
+			get_tree().change_scene_to_file("res://Views/Levels/Home.tscn")
 
