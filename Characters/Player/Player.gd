b94a3f9ad02c4504689d1ff6e924281af3f81f8e
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-04-26 11:07:01 +0200

    restructured some of the code in player

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -15,11 +15,12 @@ var carrying = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 3
-var ground
 var jump_count = 0
 # could be increased if we want a power up or for accessibly purposes
 #if we don't want to have double jump, change it to 1
 var max_jumps = 2
+var time_on_ground = 0
+var in_air = false
 
 @onready var actionableFinder: Area2D = $ActionableFinder
 
@@ -29,15 +30,70 @@ var max_jumps = 2
 @onready var animation_player = $AnimationPlayer
 
 @onready var anim = get_node("AnimationPlayer")
-var time_on_ground = 0
-
-
-
-var in_air = false
 
 func _ready():
 	# Set this node as the player node
 	Global.set_player_reference(self)
+
+func _process(_delta):
+	## Display the health of the player by a label
+	hp.text = "HP " + str(Global.health)
+
+func _physics_process(delta):
+	## Add the gravity to the player
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	## Reset the jump count when the player is on the floor/ground
+	## Add counter when is on ground, helps with the animations
+	## Otherwise, make not on ground variable true
+	if is_on_floor():
+		jump_count = 0
+		time_on_ground += 1
+	else:
+		in_air = true
+	
+	## Handle jump (including double jump)
+	if Input.is_action_just_pressed("ui_accept"):
+		jumpHandling()
+
+	#wall_sliding(delta)	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Makes it so character accelerates before hitting top speed
+	if direction and !get_tree().paused:
+		## Rotates the character depending on direction
+		rotateCharacter(direction)
+		
+		velocity.x = move_toward(velocity.x,SPEED * direction,ACCELERATION * delta)
+		
+		## Start the running animation
+		if is_on_floor() and !in_air and time_on_ground > 5:
+			anim.play("running")
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		# Play idle animation
+		if is_on_floor() and !in_air and velocity.y == 0 and velocity.x == 0 and time_on_ground > 5:
+			anim.play("idle")
+
+	move_and_slide()
+	
+	slideCollision()
+	landing()
+
+func _input(event):
+	if event.is_action_pressed("Dash"):
+		dashing()
+	
+	if event.is_action_pressed("ui_inventory"):
+		inventory()
+		
+	if (event.is_action_pressed("ui_down") and is_on_floor()):
+		position.y += 15
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("interact"):
@@ -45,62 +101,25 @@ func _unhandled_input(event):
 		if actionables.size() > 0:
 			actionables[0].action()
 			return
-	
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_P:
-			print("p is pressed")
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Makes sure the jump count is reset when the player is on the floor
-	if is_on_floor():
-		jump_count = 0
+func inventory():
+	inventory_ui.visible = !inventory_ui.visible
+	if inventory_ui.visible:
+		self.process_mode = 3
+		animation_player.stop(false)
 	else:
-		in_air = true
-	# Handle jump (including double jump)
-	if Input.is_action_just_pressed("ui_accept"):
-		if jump_count < max_jumps && Global.isCarrying == false:
-			anim.play("jump")
-			velocity.y = JUMP_VELOCITY
-			jump_count += 1
-			time_on_ground = 0
-		wall_jump()
-		
-	#wall_sliding(delta)	
+		self.process_mode = 1
+		animation_player.play()
+	get_tree().paused = !get_tree().paused
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	
-	#makes it so character accelerates before hitting top speed
-	if direction and !get_tree().paused:
-		##
-		## Rotates the character depending on direction
-		if direction == -1:
-			get_node("AnimatedSprite2D").flip_h = false
-		else:
-			get_node("AnimatedSprite2D").flip_h = true
-		velocity.x = move_toward(velocity.x,SPEED * direction,ACCELERATION * delta)
-		##
-		## Start the running animation
-		if is_on_floor() and !in_air and time_on_ground > 5:
-			anim.play("running")
+func rotateCharacter(direction):
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-		if is_on_floor() and !in_air and velocity.y == 0 and velocity.x == 0 and time_on_ground > 5:
-			anim.play("idle")
+		get_node("AnimatedSprite2D").flip_h = true
 
-	move_and_slide()
-	
-	for i in get_slide_collision_count():
-		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
-			
-	##
+func landing():
 	## Animates the landing
 	for index in get_slide_collision_count():
 		var collision = get_slide_collision(index)
@@ -108,32 +127,13 @@ func _physics_process(delta):
 			anim.play("landing")
 			in_air = false
 
-func _process(_delta):
-	hp.text = "HP " + str(Global.health)
-	
-	if is_on_floor():
-		time_on_ground += 1
-
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_D:
-			# self.position.x += 50
-			var tween = get_tree().create_tween()
-			tween.tween_property(self, "position", position + Vector2(50,0), 0.1)
-	if event.is_action_pressed("ui_inventory"):
-		inventory_ui.visible = !inventory_ui.visible
-		if inventory_ui.visible:
-			self.process_mode = 3
-			animation_player.stop(false)
-			
-		else:
-			self.process_mode = 1
-			animation_player.play()
-		get_tree().paused = !get_tree().paused
-		
-	if (event.is_action_pressed("ui_down") && is_on_floor()):
-		position.y += 15
-		
+func jumpHandling():
+	if jump_count < max_jumps && Global.isCarrying == false:
+		anim.play("jump")
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+		time_on_ground = 0
+	wall_jump()
 
 #handles wall sliding, makes it so gravity is slower when you are climbing a wall
 func wall_sliding(delta):
@@ -146,7 +146,7 @@ func wall_sliding(delta):
 		if is_wall_sliding:
 			velocity.y += (wall_slide_gravity*delta)
 			velocity.y = min(velocity.y, wall_slide_gravity)
-	
+
 #handles wall jumping, makes it so you can jump (for now infinitely) on a wall
 func wall_jump():
 	var wall_normal = get_wall_normal()
@@ -166,6 +166,11 @@ func wall_jump():
 		velocity.y = JUMP_VELOCITY
 		velocity.x = wall_normal.x * SPEED
 
+func slideCollision():
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
 
 #handles effects from items
 func apply_item_effect(item):
@@ -174,10 +179,9 @@ func apply_item_effect(item):
 			Global.change_health(5)
 		_:
 			print("There is no effect for this item")
-			
+
 ## 
 ## Pause menu functionality
-##
 func _on_resume_pressed():
 	$Camera2D/PauseMenu/Save.modulate = Color(1,1,1,1)	
 	$Camera2D/PauseMenu.hide()
@@ -189,8 +193,12 @@ func _on_save_pressed():
 	SaveGame.sceneActive = get_tree().current_scene.name
 	SaveGame.saveGame()
 	$Camera2D/PauseMenu/Save.modulate = Color(0,1,0,0.5)
-	
 
 func _on_main_menu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Main.tscn")
+
+func dashing():
+	self.position.x += 50
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", position + Vector2(50,0), 0.1)
