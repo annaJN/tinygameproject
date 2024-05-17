27d6adc22354ab101e1dad4e1ba3e0b1559b053e
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-05-17 09:51:20 +0200

    partway to fixing initiating animation of player running

--- Main.gd
+++ Main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+var initiateGame = false
+
 func _on_start_pressed():
+	initiateGame = true
 	$Message.text = "Starting Game!"
 	Global.health = 50
 	Global.positionX = 100
@@ -8,7 +11,7 @@ func _on_start_pressed():
 	Global.passedHalfway = false
 	Global.savedGame = false
 	SaveGame.saveGame()
-	get_tree().change_scene_to_file("res://Views/Levels/LevelOne.tscn")
+	#get_tree().change_scene_to_file("res://Views/Levels/LevelOne.tscn")
 
 ## A button to start the last loaded game (hopefully)
 func _on_load_pressed():
@@ -30,6 +33,25 @@ func _on_halfway_pressed():
 	Global.positionX = 2050
 	Global.positionY = 750
 	get_tree().change_scene_to_file("res://Views/Levels/LevelOne.tscn")
+
+func _process(delta):
+	if initiateGame:
+		var characterAnim = get_node("MushroomRingBackground/DummyPlayer/AnimationPlayer")
+		var character = get_node("MushroomRingBackground/DummyPlayer")
+		characterAnim.play("running")
+		while character.position.x < 1200:
+			character.position.x += 20 * delta
+			print(character.position.x)
+	
+
+func initiateGames():
+	var characterAnim = get_node("MushroomRingBackground/DummyPlayer/AnimationPlayer")
+	var character = get_node("MushroomRingBackground/DummyPlayer")
+	characterAnim.play("running")
+	while character.position.x < 1200:
+		character.position.x += 1
+		print(character.position.x)
+	#await get_tree().create_timer(2).timeout
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://Views/Menus/Settings.tscn")
